36203d95b3e12badb3b93a6fbc54c3da5b3060fb
Luis Mata <luis.antonio.mata@gmail.com>
2022-03-26 07:08:13 -0400

    PRX-59 Unlink Role and feature - Was added deleteRoleFeatureByRoleIdAndFeatureId method within RoleFeatureRepository.java file

--- src/main/java/com/prx/persistence/general/repositories/RoleFeatureRepository.java
+++ src/main/java/com/prx/persistence/general/repositories/RoleFeatureRepository.java
@@ -1,7 +1,14 @@
 package com.prx.persistence.general.repositories;
 
 import com.prx.persistence.general.domains.RoleFeatureEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface RoleFeatureRepository extends CrudRepository<RoleFeatureEntity, Long> {
+
+    @Modifying
+    @Query("Delete from RoleFeatureEntity entt where entt.role = :roleId and entt.feature = :featureId")
+    void deleteRoleFeatureByRoleIdAndFeatureId(@Param("roleId") Long roleId, @Param("featureId") Long featureId);
 }
